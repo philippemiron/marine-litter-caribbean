86f69f9657ac14f577f630e678998395420780a7
Philippe Miron <pmiron@fsu.edu>
2022-03-11 17:40:16 -0500

    read only required velocity fields files and deal with possible missing files

--- fields.py
+++ fields.py
@@ -1,9 +1,41 @@
 import glob
-import datetime
+from datetime import datetime
 from parcels import FieldSet, Field
 from os.path import join
 import numpy as np
 import xarray as xr
+
+
+def temporal_range(t: [datetime], start_date: datetime, end_date: datetime):
+    """
+    Return index of files to read in only necessary fields for the time period.
+    This function is necessary because some files are missing from the dataset, so we have to find the closest time
+    step to the start_date and end_date of the simulation.
+
+    Args:
+        t (list[datetime]): date time associated with files
+        start_date (datetime): start of time period
+        end_date (datetime): end of the time period
+
+    Returns:
+        slice: parcels object that holds velocity data for particle integration
+    """
+
+    i0 = [i for i, t in enumerate(t) if t <= start_date]
+    i1 = [i for i, t in enumerate(t) if t >= end_date]
+
+    # get closest before start_date and after end_date
+    try:
+        i0 = i0[-1]
+    except IndexError:
+        i0 = 0
+
+    try:
+        i1 = i1[0]
+    except IndexError:
+        i1 = len(t)
+
+    return slice(i0, i1 + 1)
 
 
 def hycom_fieldset(base_folder: str, start_date: datetime, end_date: datetime) -> FieldSet:
@@ -18,22 +50,18 @@ def hycom_fieldset(base_folder: str, start_date: datetime, end_date: datetime) -
     Returns: 
         FieldSet: parcels object that holds velocity data for particle integration
     """
-
-    y0 = start_date.year
-    y1 = end_date.year
-    assert (y0 >= 2010 and y0 <= 2021)
-    assert (y1 >= 2010 and y1 <= 2021)
+    assert (start_date >= datetime(2010, 1, 1))
+    assert (end_date <= datetime(2022, 1, 1))
 
     # add full years
     filenames = []
-    for year in range(y0, y1 + 1):
+    for year in range(start_date.year, end_date.year + 1):
         filenames.extend(glob.glob(join(base_folder, f'{year}', 'hycom_GLBv0.08_*.nc')))
     filenames = sorted(filenames)
 
-    # trim before and after the time period
-    i0 = [i for i, f in enumerate(filenames) if start_date.strftime("%Y%m%d") in f][0]
-    i1 = [i for i, f in enumerate(filenames) if end_date.strftime("%Y%m%d") in f][-1]
-    filenames = filenames[i0:i1 + 1]
+    # file format hycom_GLBv0.08_XXX_2021010112_t000.nc
+    filenames_t = [datetime.strptime(f[-18:-3], '%Y%m%d12_t0%H') for f in filenames]
+    filenames = filenames[temporal_range(filenames_t, start_date, end_date)]  # restrict to temporal range
 
     if len(filenames) == 0:
         raise Exception("Error: No HYCOM current file found.")
@@ -56,7 +84,7 @@ def hycom_fieldset(base_folder: str, start_date: datetime, end_date: datetime) -
         'lon': range(1012, 1615),  # 99.04ºW, -51.12ºW 1611
     }
 
-    # chunksize: 128 to 512 are typically most effective (from parcel doc)
+    # chunk size of 128 to 512 are typically most effective (from parcel doc)
     cs = {'time': ('time', 1), 'lat': ('latitude', 512), 'lon': ('longitude', 512)}
 
     fset = FieldSet.from_netcdf(filenames,
@@ -65,7 +93,7 @@ def hycom_fieldset(base_folder: str, start_date: datetime, end_date: datetime) -
                                 indices=indices,
                                 deferred_load=True,
                                 chunksize=cs,
-                                allow_time_extrapolation=True)  # avoid issue if one file is missing
+                                allow_time_extrapolation=True)  # avoid issue when a file is missing
 
     return fset
 
@@ -81,26 +109,23 @@ def jra55_fieldset(base_folder: str, start_date: datetime, end_date: datetime, k
     
     Returns: 
         FieldSet: parcels object that holds velocity data for particle integration
+    
     """
-
-    y0 = start_date.year
-    y1 = end_date.year
-    assert (y0 >= 2010 and y0 <= 2021)
-    assert (y1 >= 2010 and y1 <= 2021)
+    
+    assert (start_date >= datetime(2010, 1, 1))
+    assert (end_date <= datetime(2022, 1, 1))
 
     # add full years
     filenames = []
-    for year in range(y0, y1 + 1):
+    for year in range(start_date.year, end_date.year + 1):
         filenames.extend(glob.glob(join(base_folder, f'{year}w', 'JRA55_GLBv0.08_*.nc')))
     filenames = sorted(filenames)
 
-    # trim before and after the time period
-    i0 = [i for i, f in enumerate(filenames) if start_date.strftime("%Y%m%d") in f][0]
-    i1 = [i for i, f in enumerate(filenames) if end_date.strftime("%Y%m%d") in f][-1]
-    filenames = filenames[i0:i1 + 1]
-
+    # file format JRA55_GLBv0.08_20210101_t000.nc
+    filenames_t = [datetime.strptime(f[-16:-3], '%Y%m%d_T0%H') for f in filenames]
+    filenames = filenames[temporal_range(filenames_t, start_date, end_date)]  # restrict to temporal range
     if len(filenames) == 0:
-        raise Exception("Error: No HYCOM current file found.")
+        raise Exception("Error: No JRA55 wind file found.")
 
     # create FieldSet
     variables = {
@@ -129,7 +154,7 @@ def jra55_fieldset(base_folder: str, start_date: datetime, end_date: datetime, k
                                 indices=indices,
                                 deferred_load=True,
                                 chunksize=cs,
-                                allow_time_extrapolation=True)  # avoid issue if one file is missing
+                                allow_time_extrapolation=True)  # avoid issue when a file is missing
 
     if k_wind:
         fset.U.set_scaling_factor(k_wind)
