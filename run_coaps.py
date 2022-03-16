1df92315d83a83aa91890ffb9aba13a21a4ab27a
Philippe Miron <pmiron@fsu.edu>
2022-03-16 10:56:44 -0400

    add comments to run script

--- run_coaps.py
+++ run_coaps.py
@@ -5,14 +5,17 @@ from dateutil.relativedelta import relativedelta
 import xarray as xr
 import numpy as np
 
+#
 # make sure you activate the proper environment before launching the script
+# 
+
 # parameters
 use_mpi = True
 nb_proc = 16
-start_date = datetime(2016, 1, 1)
+start_date = datetime(2010, 1, 1)
 end_date = datetime(2022, 1, 1)
 folder = 'data/output/'
-name = 'twelve_years_winds_diffusion_2015_01'
+name = 'twelve_years_winds_diffusion_2010_01'
 winds = 'True'
 diffusion = 'True'
 unbeach = 'False'
@@ -20,10 +23,10 @@ restart_file = ''
 
 assert (start_date.day == end_date.day)
 
-i = 1  # can switch to restart
-batch_end = start_date + relativedelta(months=1)  # batch end date
+i = 1  # can modified to restart in the middle of a run
+batch_end = start_date + relativedelta(months=1)
 while start_date < end_date:
-    # to use MPI we need nb particles > nb_proc
+    # use MPI if nb particles > nb proc
     if restart_file:
         with xr.open_dataset(join(folder, restart_file)) as df:
             if sum(np.isfinite(df.lon[:,-1].values)) < nb_proc:
