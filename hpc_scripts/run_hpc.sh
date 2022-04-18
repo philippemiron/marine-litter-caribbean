99d982c51dcf3f67e4d3bb574860cd26589586de
Philippe Miron <pmiron@fsu.edu>
2022-04-18 13:32:37 -0400

    use slurm job array to launch calculations

--- hpc_scripts/run_hpc.sh
+++ hpc_scripts/run_hpc.sh
@@ -31,9 +31,9 @@ start_date_str="20${year[$i]}-${month[$i]}-01"
 
 # below is constant for all monthly releases
 end_date_str="2022-01-01"
-output_path="/gpfs/research/coaps/pmiron/caribbean-marine-litter/data/output/cm_mpw"
+output_path="/gpfs/research/coaps/pmiron/caribbean-marine-litter/data/output/cm_uniform"
 executable_path="/gpfs/research/coaps/pmiron/caribbean-marine-litter"
-basename="cm_mpw"
+basename="cm_uniform"
 days_per_run=30
 
 end_date_sec=$(date --date="$end_date_str" "+%s")
@@ -59,10 +59,10 @@ do
   if [ $t -eq  0 ]
   then
     # run from initial position
-    cmd="srun python $executable_path/caribbean_model.py $c_start_date:0 $c_end_date:0 True True False $basename_$start_date_str_$fi"
+    cmd="srun python $executable_path/caribbean_model.py $c_start_date:0 $c_end_date:0 True True False ${basename}_${start_date_str}_${fi}"
   else
     # start from restart file
-    cmd="srun python $executable_path/caribbean_model.py $c_start_date:0 $c_end_date:0 True True False $basename_$start_date_str_$fi $output_path/$basename_$start_date_str_$fp.nc"
+    cmd="srun python $executable_path/caribbean_model.py $c_start_date:0 $c_end_date:0 True True False ${basename}_${start_date_str}_${fi} ${output_path}/${basename}_${start_date_str}_${fp}.nc"
   fi
   echo $cmd
   $cmd
